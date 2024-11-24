cffe472f9a107e2ed59b50ae6527c497dbdc1946
Developer-Elisha <developer.elisha@gmail.com>
2024-11-25 03:13:55 +0500

    separating user data and admin data

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,31 @@
-import React, { useContext, useEffect, useState } from 'react'
-import Login from './components/Auth/Login'
-import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
-import AdminDashboard from './components/Dashboard/AdminDashboard'
-import { getLocalStorage, setLocalStorage } from './utils/localStorage'
-import { AuthContext } from './context/AuthProvider'
+import React, { useContext, useState } from 'react';
+import Login from './components/Auth/Login';
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard';
+import AdminDashboard from './components/Dashboard/AdminDashboard';
+import { AuthContext } from './context/AuthProvider';
 
 const App = () => {
-  
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
+  const userData = useContext(AuthContext);
 
   const handleLogin = (email, password) => {
-    if(email == 'admin@gmail.com' && password == '123'){
-      setUser("admin")
+    if (userData?.admin?.email === email && userData?.admin?.password === password) {
+      setUser('admin');
     }
-    else if(email == 'employee@gmail.com' && password == '123'){
-      setUser("employee")
+    else if (userData?.employees && userData.employees.find((e) => e.email === email && e.password === password)
+    ) {
+      setUser('employee');
+    } else {
+      alert('Invalid Credentials');
     }
-    else{
-      alert("Invalid Credentials");
-    }
-  }
-
-  const data = useContext(AuthContext)
-  console.log(data);
-  
+  };
 
   return (
     <>
-    {!user ? <Login handleLogin={handleLogin} /> : ''}
-    {user == 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
+      {!user ? <Login handleLogin={handleLogin} /> : ''}
+      {user === 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,13 +1,22 @@
-import React, { createContext } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
+import { getLocalStorage } from '../utils/localStorage'
 
-export const AuthContext= createContext()
+export const AuthContext = createContext()
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+
+  const [userData, setUserData] = useState(null)
+
+  useEffect(() => {
+    const {employees, admin} = getLocalStorage()
+    setUserData({employees, admin})
+  }, [])
+
   return (
     <div>
-        <AuthContext.Provider value={"Elisha"}>
-            {children}
-        </AuthContext.Provider>
+      <AuthContext.Provider value={userData}>
+        {children}
+      </AuthContext.Provider>
     </div>
   )
 }
